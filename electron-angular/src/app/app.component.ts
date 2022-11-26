e0598d428295d9a17a86b5ab69d2e3105bdf6f70
Daniel Montilla <montilla.d2000@gmail.com>
2022-11-26 15:02:06 -0400

    changed names

---
+++ electron-angular/src/app/app.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'sample-an';
+  count: number = 0;
+
+  increment() {
+    this.count++;
+  }
+}
